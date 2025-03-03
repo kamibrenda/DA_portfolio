d5871c40e3c63e30f5cedeaec19c2105c62f1d72
kamibrenda <mukamibrenda254@gmail.com>
2025-03-03 20:33:58 +0300

    where up

--- SQL/layoffs project/where.sql
+++ SQL/layoffs project/where.sql
@@ -20,3 +20,20 @@ FROM
 parks_and_recreation.employee_demographics
 WHERE birth_date > '1985-01-01'
 AND gender = 'male';
+
+SELECT*
+FROM
+parks_and_recreation.employee_demographics
+WHERE birth_date > '1985-01-01'
+OR NOT gender = 'male';
+
+SELECT *
+FROM employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44)
+		OR age > 55;
+        
+-- LIKE st
+-- %(any value) and _ (specific value)
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'Jer'  -- get the exact match

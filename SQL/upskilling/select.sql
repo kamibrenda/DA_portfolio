a7f4bf8c76f955a703badb178138ca245ccd7b17
kamibrenda <mukamibrenda254@gmail.com>
2025-05-08 11:07:11 +0300

    practice workflow-basic

--- SQL/upskilling/select.sql
+++ SQL/upskilling/select.sql
@@ -1,1 +1,8 @@
-select * from students
+-- to get the DB being queried
+USE maven_advanced_sql;
+
+-- with the explanation of workchart flow 
+select * 
+from students   -- table selected to 
+where gpa > 3.0    -- filter out based on the selected condition
+order by grade_level desc  -- arrange by largest to smallest 
